1a32299de9a14ecaace94856acdc4431615cde85
WarHatch <satelistas@gmail.com>
2018-10-16 00:51:43 +0300

    main tests

--- src/Main.scala
+++ src/Main.scala
@@ -2,13 +2,33 @@ import helpers.Vector2
 
 object Main extends App {
   val dummyWarrior = new HealthySwordsman(30, new Vector2(1, 2))
+
+
   var swordheal = new HealthySwordsman(100, new Vector2())
   swordheal.Pray
   swordheal.Pray
   swordheal.Pray
   swordheal.GetBonuses(swordheal)
 
-
   swordheal.Approach(swordheal, dummyWarrior)
   swordheal.Attack(swordheal, dummyWarrior)
+
+
+  var healthyArcher = new HealthyArcher(100, new Vector2(1, 1))
+  healthyArcher.Pray
+  healthyArcher.Pray
+  healthyArcher.Pray
+  healthyArcher.GetBonuses(healthyArcher)
+
+  healthyArcher.Approach(healthyArcher, dummyWarrior)
+  healthyArcher.Attack(healthyArcher, dummyWarrior)
+
+  var resurrectingCavalry = new ResurrectingCavalry(100, new Vector2(1, 1))
+  for (a <- 1 to 10) {
+    swordheal.Pray
+  }
+  resurrectingCavalry.GetBonuses(resurrectingCavalry)
+
+  resurrectingCavalry.Approach(resurrectingCavalry, dummyWarrior)
+  resurrectingCavalry.Attack(resurrectingCavalry, dummyWarrior)
 }
